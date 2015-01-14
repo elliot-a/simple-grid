e0977012675e5cd156355f69a41546863ff2e397
Elliot Agro <elliotagro83@gmail.com>
2015-01-14 15:17:41 +0000

    changed message

--- output/app.js
+++ output/app.js
@@ -4,21 +4,21 @@ var testData = {
     values: ['mentions_17283728', 'mentions_17283729' ],
     categories: ['18/01/2015', '19/01/2015', '20/01/2015'],
     labels: {
-        'mentions_17283728':'"muslim" on the dailymail.co.uk',
+        'mentions_17283728':'"hate" on the dailymail.co.uk',
         'mentions_17283729':'"terror" on the dailymail.co.uk'
     },
     data: [
         {
             'mentions_17283728' : 131,
-            'mentions_17283729' : 94,
+            'mentions_17283729' : 94
         },
         {
             'mentions_17283728' : 130,
-            'mentions_17283729' : 35,
+            'mentions_17283729' : 35
         },
         {
             'mentions_17283728': 33,
-            'mentions_17283729' : 93,
+            'mentions_17283729' : 93
         }
     ]
 };
